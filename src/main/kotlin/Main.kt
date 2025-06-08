cb9c964e494f826d94b31b481998d634034c9ed7
Nikita Torop <torop.nikita1@gmail.com>
2025-06-08 18:51:45 +0300

    Add Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,25 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import java.io.File
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+suspend fun main() {
+    val workingDir = System.getProperty("user.home") +
+            File.separator + "Downloads" +
+            File.separator + "ImagesWithKotlin"
+    val linksPath = workingDir + File.separator + "Links"
+    val downloadPath = workingDir + File.separator + "DownloadedImages"
+    val resizedPath = workingDir + File.separator + "ResizedImages"
+    println("Processing is about to start. Config:\nLinks folder: $linksPath\nDownload folder: $downloadPath\nResized folder: $resizedPath")
+
+    val pathsToFilesWithLinks = listOf(
+        linksPath + File.separator + "dynamic.txt",
+    )
+    val pathsToFilesWithIgnoreLinks = listOf(
+        linksPath + File.separator + "ignore_links.txt",
+    )
+    val downloadLinks = read(pathsToFilesWithLinks)
+    val ignoreLinks = read(pathsToFilesWithIgnoreLinks)
+    val filteredLinks = filter(downloadLinks, ignoreLinks)
+    val downloadedFiles = download(filteredLinks, downloadPath)
+    resize(downloadedFiles, resizedPath)
 }
